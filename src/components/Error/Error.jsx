65dfab225a38dabc7626225f7b54fcdfc10ece9a
carmantegazza <carmantegazza@gmail.com>
2023-01-17 01:51:34 -0300

    comp error ok

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,11 @@
+import CartButtonToShopping from "../CartButtonToShopping/CartButtonToShopping"
+
 const Error = () => {
   return (
     <div className="text-center p-2">
         <img className="img-thumbnail" src="https://res.cloudinary.com/dx8uk8mjq/image/upload/v1670670354/hyungwonMeme_qz3pn9.gif" alt="Hyungwon Coffee Meme" />
         <p className="fw-semibold">Oops, no hay nada por aqui...</p>
+        <CartButtonToShopping text={"Volver al inicio"} />
     </div>
   )
 }
